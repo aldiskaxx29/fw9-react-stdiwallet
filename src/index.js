8cb00fd586a9fb766b9cc224c75275c9c8f32d95
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-22 16:56:19 +0700

    fix: add import css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './assets/css/style1.css'
+import './assets/css/style2.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
