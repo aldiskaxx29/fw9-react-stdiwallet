58ee3b28ecc9ee5da313d54ca755bf0db78c7b55
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-22 16:55:54 +0700

    feat: add new route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
-import Landingpage from "./pages/landingpage";
+import Header from "./component/Headers";
+import CreatePin from "./pages/CreatePin";
+import CreatePinSuccess from "./pages/CreatePinSuccess";
+import ForgotPassword from "./pages/ForgotPassword";
+import ForgotPasswordInsert from "./pages/ForgotPasswordInsert";
+import Landingpage from "./pages/Landingpage";
+import  Login  from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="landingpage" element={<Landingpage/>}/>
-        <Route/>
+        <Route path="/" element={<Landingpage/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="signUp" element={<SignUp/>}/>
+        <Route path="createPin" element={<CreatePin/>}/>
+        <Route path="createPinSuccess" element={<CreatePinSuccess/>}/>
+        <Route path="forgotPassword" element={<ForgotPassword/>}/>
+        <Route path="forgotPasswordInsert" element={<ForgotPasswordInsert/>}/>
+        <Route path="home" element={<Header/>}/>
       </Routes>
     </BrowserRouter>
   );
