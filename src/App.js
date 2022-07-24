368a6fa26823f8cc3996abca31a8839e7865d847
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-24 14:09:37 +0700

    add: new route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import { AddNumber } from "./pages/AddNumber";
+import { ChangePassword } from "./pages/ChangePassword";
+import { ChangePin } from "./pages/ChangePin";
+import { ChangePinNew } from "./pages/ChangePinNew";
 import CreatePin from "./pages/CreatePin";
 import CreatePinSuccess from "./pages/CreatePinSuccess";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -7,6 +11,8 @@ import ForgotPasswordInsert from "./pages/ForgotPasswordInsert";
 import { Home } from "./pages/Home";
 import Landingpage from "./pages/Landingpage";
 import  Login  from "./pages/Login";
+import { ManagePhone } from "./pages/ManagePhone";
+import { PersonalInfo } from "./pages/PersonalInfo";
 import { Profile } from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import { StatusFailed } from "./pages/StatusFailed";
@@ -35,6 +41,12 @@ const App = () => {
         <Route path="pinConfirm" element={<TransferPinConfirm/>}/>
         <Route path="statusSuccess" element={<StatusSuccess/>}/>
         <Route path="statusFailed" element={<StatusFailed/>}/>
+        <Route path="personalInfo" element={<PersonalInfo/>}/>
+        <Route path="manageNumber" element={<ManagePhone/>}/>
+        <Route path="addNumber" element={<AddNumber/>}/>
+        <Route path="changePassword" element={<ChangePassword/>}/>
+        <Route path="changePin" element={<ChangePin/>}/>
+        <Route path="typeNewPin" element={<ChangePinNew/>}/>
       </Routes>
     </BrowserRouter>
   );
