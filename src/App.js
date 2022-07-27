56d7172fb4efdf1b53afe69ecb3fa4bc332151f7
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-27 17:01:52 +0700

    feat: add Private Route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from "react";
+import React from 'react';
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
-import { AddNumber } from "./pages/AddNumber";
-import { ChangePassword } from "./pages/ChangePassword";
-import { ChangePin } from "./pages/ChangePin";
-import { ChangePinNew } from "./pages/ChangePinNew";
-import CreatePin from "./pages/CreatePin";
-import CreatePinSuccess from "./pages/CreatePinSuccess";
-import ForgotPassword from "./pages/ForgotPassword";
-import ForgotPasswordInsert from "./pages/ForgotPasswordInsert";
-import { History } from "./pages/History";
-import { Home } from "./pages/Home";
-import Landingpage from "./pages/Landingpage";
-import  Login  from "./pages/Login";
-import { ManagePhone } from "./pages/ManagePhone";
-import { PersonalInfo } from "./pages/PersonalInfo";
-import { Profile } from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import { StatusFailed } from "./pages/StatusFailed";
-import { StatusSuccess } from "./pages/StatusSuccess";
-import { TopUp } from "./pages/TopUp";
-import Transfer from "./pages/Transfer";
-import { TransferInput } from "./pages/TransferInput";
-import { TransferPinConfirm } from "./pages/TransferPinConfirm";
+import { PrivateRoute } from './component/PrivateRoute';
+import { AddNumber } from './pages/AddNumber';
+import { ChangePassword } from './pages/ChangePassword';
+import { ChangePin } from './pages/ChangePin';
+import { ChangePinNew } from './pages/ChangePinNew';
+import CreatePin from './pages/CreatePin';
+import CreatePinSuccess from './pages/CreatePinSuccess';
+import ForgotPassword from './pages/ForgotPassword';
+import ForgotPasswordInsert from './pages/ForgotPasswordInsert';
+import { History } from './pages/History';
+import { Home } from './pages/Home';
+import Landingpage from './pages/Landingpage';
+import  Login  from './pages/Login';
+import { ManagePhone } from './pages/ManagePhone';
+import { PersonalInfo } from './pages/PersonalInfo';
+import { Profile } from './pages/Profile';
+import SignUp from './pages/SignUp';
+import { StatusFailed } from './pages/StatusFailed';
+import { StatusSuccess } from './pages/StatusSuccess';
+import { TopUp } from './pages/TopUp';
+import Transfer from './pages/Transfer';
+import { TransferInput } from './pages/TransferInput';
+import { TransferPinConfirm } from './pages/TransferPinConfirm';
 
 const App = () => {
   return (
@@ -34,21 +35,96 @@ const App = () => {
         <Route path="createPinSuccess" element={<CreatePinSuccess/>}/>
         <Route path="forgotPassword" element={<ForgotPassword/>}/>
         <Route path="forgotPasswordInsert" element={<ForgotPasswordInsert/>}/>
-        <Route path="home" element={<Home/>}/>
-        <Route path="history" element={<History/>}/>
-        <Route path="transfer" element={<Transfer/>}/>
-        <Route path="topUp" element={<TopUp/>}/>
-        <Route path="profile" element={<Profile/>}/>
-        <Route path="transferInput" element={<TransferInput/>}/>
-        <Route path="pinConfirm" element={<TransferPinConfirm/>}/>
-        <Route path="statusSuccess" element={<StatusSuccess/>}/>
-        <Route path="statusFailed" element={<StatusFailed/>}/>
-        <Route path="personalInfo" element={<PersonalInfo/>}/>
-        <Route path="manageNumber" element={<ManagePhone/>}/>
-        <Route path="addNumber" element={<AddNumber/>}/>
-        <Route path="changePassword" element={<ChangePassword/>}/>
-        <Route path="changePin" element={<ChangePin/>}/>
-        <Route path="typeNewPin" element={<ChangePinNew/>}/>
+        
+        <Route path="home" element=
+          {<PrivateRoute>
+            <Home/>
+          </PrivateRoute>
+          }/>
+
+        <Route path="history" element={
+          <PrivateRoute>
+            <History/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="transfer" element={
+          <PrivateRoute>
+            <Transfer/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="topUp" element={
+          <PrivateRoute>
+            <TopUp/>
+          </PrivateRoute>
+        }/>
+        
+        <Route path="profile" element={
+          <PrivateRoute>
+            <Profile/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="transferInput" element={
+          <PrivateRoute>
+            <TransferInput/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="pinConfirm" element={
+          <PrivateRoute>
+            <TransferPinConfirm/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="statusSuccess" element={
+          <PrivateRoute>
+            <StatusSuccess/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="statusFailed" element={
+          <PrivateRoute>
+            <StatusFailed/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="personalInfo" element={
+          <PrivateRoute>
+            <PersonalInfo/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="manageNumber" element={
+          <PrivateRoute>
+            <ManagePhone/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="addNumber" element={
+          <PrivateRoute>
+            <AddNumber/>
+          </PrivateRoute>
+        }/>
+        
+        <Route path="changePassword" element={
+          <PrivateRoute>
+            <ChangePassword/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="changePin" element={
+          <PrivateRoute>
+            <ChangePin/>
+          </PrivateRoute>
+        }/>
+
+        <Route path="typeNewPin" element={
+          <PrivateRoute>
+            <ChangePinNew/>
+          </PrivateRoute>
+        }/>
       </Routes>
     </BrowserRouter>
   );
