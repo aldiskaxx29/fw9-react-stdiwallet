ea550fdae1cf027c202d7f891d09d6f85d11ec8f
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-29 17:02:13 +0700

    feat: change condition

--- src/component/PrivateRoute.js
+++ src/component/PrivateRoute.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux/es/exports'
 
 export const PrivateRoute = ({children}) => {
-  const auth = localStorage.getItem('auth')
+  const auth = useSelector((state=>state.auth.token))
   return auth? (
     children
   ):(<Navigate to='/login' replace={true} state={{errormsg: 'You Must Login to Access Page'}}/>)
--- src/component/PublicRoute.js
+++ src/component/PublicRoute.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux/es/exports'
 
 export const PublicRoute = ({children}) => {
-  const auth = localStorage.getItem('auth')
+  const auth = useSelector((state=>state.auth.token))
   return !auth? (
     children
   ):(<Navigate to='/home' replace={true}/>)
