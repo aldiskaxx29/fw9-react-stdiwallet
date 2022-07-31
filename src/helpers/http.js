9f94172d1a3255747d86cdfa7e11562710bc38d7
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-08-01 05:23:35 +0700

    feat: add headers and base url

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
-const http = () => {
-    
+const http = (token) => {
+  const headers ={}
+  if(token){
+    headers.authorization = `Bearer ${token}`
+  }
+  return axios.create({
+    headers,
+    baseURL:'http://localhost:3333'
+  })
 }
+
+export default http
